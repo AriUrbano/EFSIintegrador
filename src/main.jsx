42a743b32f1ee176ae67de4ab0960411b866610a
AriUrbano <48113061@est.ort.edu.ar>
2025-11-04 15:57:28 -0300

    Correcion de eliminacion de datos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AppProvider } from './context/AppContext';
+import { TemaProvider } from './context/TemaContext';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TemaProvider>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </TemaProvider>
+  </React.StrictMode>
+);
